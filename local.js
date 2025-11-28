c7b29fcd157b37e46efa0d19c6a8b508d7b36233
Thomas J. Jonah <thomasjjonah@gmail.com>
2025-11-28 04:43:33 -0500

    Update local.js

--- local.js
+++ local.js
@@ -37,8 +37,10 @@ function toggleMenu() {
       dropdown_buttons_text = dropdown_buttons.text(),
       home_button = $('.header > .menu .dropdown .home'),
       home_section_container = $('body > .content .section-container.home'),
-      latest_work_button = $('.header > .menu .dropdown .latest-work'),
-      latest_work_section_container = $('body > .content .section-container.latest-work'),
+      latest_photographs_button = $('.header > .menu .dropdown .latest-photographs'),
+      latest_photographs_section_container = $('body > .content .section-container.latest-photographs'),
+      mixed_reality_button = $('.header > .menu .dropdown .mixed-reality'),
+      mixed_reality_section_container = $('body > .content .section-container.mixed-reality'),
       tableaux_button = $('.header > .menu .dropdown .tableaux'),
       tableaux_section_container = $('body > .content .section-container.tableaux'),
       photo_series_button = $('.header > .menu .dropdown .photo-series'), 
@@ -79,8 +81,13 @@ function toggleMenu() {
         $('body > .content .section-container.home').addClass('show');
       }
       
-      if ($(this).hasClass('latest-work')) {
-        $('body > .content .section-container.latest-work').addClass('show');
+      if ($(this).hasClass('latest-photographs')) {
+        $('body > .content .section-container.latest-photographs').addClass('show');
+        header_title.addClass('short');
+      }
+      
+      if ($(this).hasClass('mixed-reality')) {
+        $('body > .content .section-container.mixed-reality').addClass('show');
         header_title.addClass('short');
       }
       
